f67c6e786a264e495ea0cfffe47c63bd8343d60a
jfgwell <157252721+jfgwell@users.noreply.github.com>
2024-01-20 20:41:43 -0500

    Add files via upload

---
+++ SubReddit/Program.cs
@@ -0,0 +1,17 @@
+﻿using SubRedditLogic;
+
+namespace SubReddit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+
+            Interval I = new Interval();
+            I.Setup();
+            I.Run();
+        }
+    }
+}
