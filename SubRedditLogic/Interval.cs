f954580ca01f0ad059c47a5ea0d5e5f3d9a11722
jfgwell <157252721+jfgwell@users.noreply.github.com>
2024-01-20 20:42:24 -0500

    Add files via upload

---
+++ SubRedditLogic/Interval.cs
@@ -0,0 +1,30 @@
+﻿namespace SubRedditLogic
+{
+    public class Interval
+    {
+        private static System.Timers.Timer timer;
+        private static SubRedditLogic redditLogic;
+
+        public void Setup()
+        {
+            redditLogic = new SubRedditLogic();
+            redditLogic.Setup();
+        }
+        public void Run()
+        {
+            Timer timer = new Timer(TimerCallback, null, TimeSpan.Zero, TimeSpan.FromSeconds(5));
+
+
+            // Keep the program running
+            Console.WriteLine("Press Enter to exit.");
+            Console.ReadLine();
+        }
+
+
+        private static void TimerCallback(object state)
+        {
+            // Call the Process() method at the specified interval
+            redditLogic.Process();
+        }
+    }
+}
---
+++ SubRedditLogic/SubRedditLogic.cs
@@ -0,0 +1,32 @@
+﻿using Reddit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SubRedditLogic
+{
+    public class SubRedditLogic
+    {
+        private Reddit.RedditClient reddit;
+        private List<Reddit.Controllers.Subreddit> subreddits;
+        private List<Reddit.Controllers.Post> toppostsofalltime;
+        public void Setup()
+        {
+            reddit = new RedditClient("_gPgpfkDiWqwtvBnVoUUEA", null, null, "eyJhbGciOiJSUzI1NiIsImtpZCI6IlNIQTI1NjpzS3dsMnlsV0VtMjVmcXhwTU40cWY4MXE2OWFFdWFyMnpLMUdhVGxjdWNZIiwidHlwIjoiSldUIn0.eyJzdWIiOiJ1c2VyIiwiZXhwIjoxNzA1ODc1MDE4LjI0NDcyOSwiaWF0IjoxNzA1Nzg4NjE4LjI0NDcyOSwianRpIjoicUN3SXNwalRpRGFJazJxbHVtM1NTZG1VelF5M1Z3IiwiY2lkIjoiX2dQZ3Bma0RpV3F3dHZCblZvVVVFQSIsImxpZCI6InQyX2tta2o0ZHVuZyIsImFpZCI6InQyX2tta2o0ZHVuZyIsImxjYSI6MTY5NTg2NjE2ODk2NSwic2NwIjoiZUp5S1Z0SlNpZ1VFQUFEX193TnpBU2MiLCJmbG8iOjl9.TdyQWQlxqS48MUjHJJsxRBh25y0coeueIlfPTJeNahIbvLPb4YxGpSAkNKQCfFKAK3LW5_qLQUu0kHfvj3jK1V57b7SBeU62Em24zWB9wWrSR0OtLy4AOiqSZsq2EeHbsZ2IjeLpXQdD2Hjv0NqoGP9sAsvwJZ8QHRuCHDfK0UE0R1TsIt7nZ1eVE6yaaclktvhccTGftfQ7RXmK5Ozdtf5td3KaAl3qGArsqRCsWFntuBHS3Gm1-hmSy1M5x98TxlFho42P56r2VYXg7wfKxlVMwmHhxlQ7O96VeFgYuSOlT18R0txm_cGxV1ZQsE_w2CBuTiACi02TnW9tE5p0vg", null);
+            subreddits = reddit.Account.MySubscribedSubreddits();
+            toppostsofalltime = subreddits[0].Posts.GetTop("all");
+        }
+
+
+        public void Process()
+        {
+            Console.WriteLine(subreddits[0].Name);
+
+            Console.WriteLine(toppostsofalltime.First().UpVotes.ToString());
+
+            Console.WriteLine(subreddits[0].Posts.GetTop("all").First().Author);
+        }
+    }
+}
